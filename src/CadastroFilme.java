0a80c4d45546ca0f810299bb02a58ee8f0da159f
José Anderson da Silva Costa <60760405+DessimA@users.noreply.github.com>
2022-04-22 19:26:40 -0300

    Add files via upload

---
+++ src/CadastroFilme.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CadastroFilme {
+	private List<Filme> filmes = new ArrayList<Filme>();
+
+	
+	public void adicionarFilme(Filme filme) {
+		this.filmes.add(filme);
+	}
+	
+	public void removerFilme(Filme filme) {
+		this.filmes.remove(filme);
+	}
+	
+	public void imprimirFilmes() {
+		for (Filme filme : filmes) {
+			System.out.println("Titulo: " + filme.getTitulo());
+			System.out.println("Categoria: " + filme.getCategoria());
+			System.out.println("Duracao: " + filme.getDuracao()+" min");
+			System.out.println("Preco:R$" + filme.getPreco());
+		}
+	}
+}
